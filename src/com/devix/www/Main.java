8acec771db215dc7926ccadf7f38d23c8fb04110
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-07-30 11:01:07 -0500

    Merge two arrays

---
+++ src/com/devix/www/Main.java
@@ -0,0 +1,17 @@
+package com.devix.www;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a[] = {"A", "E", "I"};
+        String b[] = {"O", "U"};
+        List list = new ArrayList(Arrays.asList(a));
+        list.addAll(Arrays.asList(b));
+        Object[] c = list.toArray();
+        System.out.println(Arrays.toString(c));
+    }
+}
